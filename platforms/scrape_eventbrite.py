04aa64d098c57864b1e8fc02895bab78eec6e743
PolBaladas <polbaladasluna@gmail.com>
2016-12-04 13:29:23 +0100

    ScrapeEvents using loklak scraping API and write out results as JSON file

--- platforms/scrape_eventbrite.py
+++ platforms/scrape_eventbrite.py
@@ -1,6 +1,10 @@
 import sys,json,requests
 from bs4 import BeautifulSoup
 
+scrape_api = "http://loklak.org/api/eventbritecrawler.json?url="
+search_url = "https://www.eventbrite.com/d/worldwide/query/?crt=regular&sort=best" 
+#replace "query" with user's query 
+
 def makeSoup(url):
 	pass
 
@@ -8,7 +12,13 @@ def getEventsLinks(soup):
 	pass
 
 def scrapeEvents(urls):
-	pass
+	for url in urls:
+		json_data = requests.get(scrape_api+url).json()
+		writeOut(json_data)
+
+def writeOut(data):
+	with open('results.json', 'w') as f:
+		json.dump(data, f)
 
 if len(sys.argv)>1:
 	query = sys.argv[1]
